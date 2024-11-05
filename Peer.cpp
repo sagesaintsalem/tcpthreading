593c01e3fca12eaf081081559fc4f7e73d7e3524
sagesaintsalem <ms.r.toussaint@gmail.com>
2024-11-05 08:53:03 +0000

    Still doesn't work

--- Peer.cpp
+++ Peer.cpp
@@ -1,9 +1,9 @@
 
 #include "Peer.h"
 
-void Peer::startConnection(){
-	int acceptorPort = stoi(port);
-	tcp::acceptor acceptor(io_ctx, tcp::endpoint(tcp::v4(), acceptorPort)); //Accepts connections
+void Peer::startConnection(){ // This sets up the P2P connection before running readMessage and sendMessage simultaneously.
+	int acceptorPort = stoi(port); //Converts port string to int because tcp::endpoint only accepts int values as a second parameter.
+	tcp::acceptor acceptor(io_ctx, tcp::endpoint(tcp::v4(), acceptorPort)); //Accepts connections using IPv4 TCP protocol.
 	cout << "Hello! Please enter your handle: ";
 	getline(cin, name);
 	cout << "Hi " << name << "! Waiting for peer...\n";
@@ -21,9 +21,9 @@ void Peer::startConnection(){
 }
 
 
-void Peer::connectToSender() {
-	tcp::resolver::results_type endpoints = resolver.resolve("127.0.0.1", port);
-	connect(socket, endpoints);
+void Peer::connectToSender() { // This connects to existing P2P chat before running readMessage and sendMessage simultaneously.
+	tcp::resolver::results_type endpoints = resolver.resolve("127.0.0.1", port); //Resolves host and service names into a list of endpoint entries.
+	connect(socket, endpoints); // Connects to socket.
 	cout << "Please enter your name: ";
 	getline(cin, name);
 	cout << "Welcome " << name << "!\n";
@@ -42,7 +42,6 @@ void Peer::sendMessage() {
 	std::getline(cin, message);
 	message = "\n" + name + ": " + message + "\n";
 	auto message_pointer = std::make_shared<string>(message);
-	/*boost::asio::write(socket, boost::asio::buffer(message), error);*/
 	async_write(socket, buffer(*message_pointer), [message_pointer](boost::system::error_code error, size_t) {
 		if (error) {
 			cout << "Error sending message" << endl;
@@ -51,10 +50,6 @@ void Peer::sendMessage() {
 }
 
 void Peer::readMessage() {
-	//std::array<char, 240> buf{};
-	/*size_t len = socket.read_some(boost::asio::buffer(buf), error);
-	cout.write(buf.data(), len);
-	cout << "\n";*/
 	async_read_until(socket, buffer_, "\n", [this](boost::system::error_code error, size_t len) {
 		std::istream stream(&buffer_);
 		string message;
--- Peer.h
+++ Peer.h
@@ -4,19 +4,19 @@
 class Peer {
 private:
 	string port;
-	streambuf buffer_;
+	streambuf buffer_; 
 public:
 	Peer(string port) {
 		setPort(port);
 	};
 	 
-	io_context io_ctx; 
+	io_context io_ctx; // Provides input/output functionality.
 	string name;
 	tcp::socket socket{ io_ctx };
-	tcp::resolver resolver{ io_ctx };
-	void setPort(string portNumber) { port = portNumber; }
+	tcp::resolver resolver{ io_ctx }; // Connects to existing connection.
+	void setPort(string portNumber) { port = portNumber; } // Sets the port number for use in startConnection and connectToSender.
 
-	inline void startConnection();
+	inline void startConnection(); //Body of this function and the next three are in Peer.cpp.
 
 	inline void connectToSender();
 	
